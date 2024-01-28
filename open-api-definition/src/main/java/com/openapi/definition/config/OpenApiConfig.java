c42ab98ab1e8c8854049ae5c66482e97d56fc8f3
wassubaba <waseem.cmc@gmail.com>
2024-01-28 18:33:50 +0530

    OpenApi 3 Security Implementation
    OpenApi 3 Security Implementation

--- open-api-definition/src/main/java/com/openapi/definition/config/OpenApiConfig.java
+++ open-api-definition/src/main/java/com/openapi/definition/config/OpenApiConfig.java
@@ -1,9 +1,13 @@
 package com.openapi.definition.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
@@ -20,9 +24,20 @@ import io.swagger.v3.oas.annotations.servers.Server;
 		servers = {
 				@Server(description = "Dev", url = "http://localhost:8080"),
 				@Server(description = "Test", url = "http://localhost:8080")
-				}
+				},
+		
+		security = @SecurityRequirement(name = "basicAuth")   // Implemented Global Security 
+		
 		
 )
+
+@SecurityScheme ( 
+		name = "basicAuth",
+		in = SecuritySchemeIn.DEFAULT,
+		type = SecuritySchemeType.HTTP,
+		scheme = "basic",
+		description = "Security Description"
+		)
 	
 public class OpenApiConfig {
 
